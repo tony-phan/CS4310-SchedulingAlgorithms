6f2d3f32cf9fd2c07228e12bba4be9422ff03955
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-03-17 00:16:41 -0700

    Refined helper functions

--- src/Job.java
+++ src/Job.java
@@ -1,7 +1,9 @@
-public class Job {
+public class Job implements Comparable<Job> {
     
     private String name;
     private int burstTime;
+    private int startTime;
+    private int endTime;
     
     public Job(String name, int burstTime) {
         this.name = name;
@@ -12,7 +14,23 @@ public class Job {
         return burstTime;
     }
 
+    @Override
     public String toString() {
         return name + "," + burstTime;
+    }
+
+    //compare the burst time of the job with another job
+    @Override
+    public int compareTo(Job job) {
+        // TODO Auto-generated method stub
+        if(this.burstTime < job.burstTime) {
+            return -1;
+        }
+        else if(this.burstTime == job.burstTime) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,32 @@
 import java.util.*;
-
-import javax.print.attribute.standard.JobSheets;
-
 import java.io.*;
 
 public class Main {
-    final static int[] JOB_SIZES = {5, 10, 15};
+    final static int[] JOB_SIZES = {5, 10, 15, 20, 25, 30};
+    final static String FILE_PATH = "C:\\Users\\tonyp\\Documents\\CS 4310\\Project1\\src\\jobs.txt";
+
     public static void main(String[] args) throws Exception {
         List<Job> jobList = new ArrayList<Job>();
-        for(int jobSize : JOB_SIZES) {
-            generateRandomJobsInFile(jobSize);
-            readJobFile(jobList);
+        readJobFile(FILE_PATH, jobList);
 
-            double firstComeFirstServed = fcfs(new ArrayList<Job>(jobList));
-            double shorstJobFirst = sjf(new ArrayList<Job>(jobList));
-            double roundRobin2 = roundRobin(2, new ArrayList<Job>(jobList));
-            double roundRobin5 = roundRobin(5, new ArrayList<Job>(jobList));
-            
-            jobList.clear();
-        }
+        double firstComeFirstServed = fcfs(new ArrayList<Job>(jobList));
+        double shorstJobFirst = sjf(new ArrayList<Job>(jobList));
+        double roundRobin2 = roundRobin(2, new ArrayList<Job>(jobList));
+        double roundRobin5 = roundRobin(5, new ArrayList<Job>(jobList));
     }
 
     private static double fcfs(List<Job> jobs) {
+        System.out.println("Performing First Come First Serve Scheduling...");
+        Queue<Job> queue = new LinkedList<Job>(jobs);
+
         double turnAroundTime = 0;
 
         return turnAroundTime;
     }
 
     private static double sjf(List<Job> jobs) {
-        jobs = sortJobs(jobs);
+        System.out.println("Performing Shortest Job First Scheduling...");
+        PriorityQueue<Job> priorityQueue = new PriorityQueue<Job>(jobs);
 
         double turnAroundTime = 0;
 
@@ -36,6 +34,7 @@ public class Main {
     }
 
     private static double roundRobin(int timeSlice, List<Job> jobs) {
+        System.out.println("Performing Round Robin (Time Slice: " + timeSlice + ") Scheduling...");
         double turnAroundTime = 0;
 
         return turnAroundTime;
@@ -59,9 +58,9 @@ public class Main {
         }
     }
 
-    private static void readJobFile(List<Job> jobList) {
+    private static void readJobFile(String file, List<Job> jobList) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("jobs.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(file));
             String line = reader.readLine();
             while(line != null) {
                 String name = line;
@@ -76,51 +75,6 @@ public class Main {
         } catch (IOException e) {
             System.out.println("An error occured");
             e.printStackTrace();
-        }
-    }
-
-    private static List<Job> sortJobs(List<Job> jobList) {
-        Job[] jobArray = jobList.toArray(new Job[0]);
-        mergeSort(jobArray, jobArray.length);
-
-        return Arrays.asList(jobArray);
-    }
-
-    private static void mergeSort(Job[] jobList, int len) {
-        if(len < 2) {
-            return;
-        }
-
-        int mid = len / 2;
-        Job[] left = new Job[mid];
-        Job[] right = new Job[len - mid];
-        for(int i = 0; i < mid; ++i) {
-            left[i] = jobList[i];
-        }
-        for(int i = mid; i < len; ++i) {
-            right[i - mid] = jobList[i];
-        }
-
-        mergeSort(left, mid);
-        mergeSort(right, len - mid);
-
-        merge(jobList, left, right, mid, len - mid);
-    }
-    private static void merge(Job[] jobList, Job[] leftSubarray, Job[] rightSubarray, int left, int right) {
-        int i = 0, j = 0, k = 0;
-        while(i < left && j < right) {
-            if(leftSubarray[i].getBurstTime() <= rightSubarray[j].getBurstTime()) {
-                jobList[k++] = leftSubarray[i++];
-            }
-            else {
-                jobList[k++] = rightSubarray[j++];
-            }
-        }
-        while(i < left) {
-            jobList[k++] = leftSubarray[i++];
-        }
-        while(j < right) {
-            jobList[k++] = rightSubarray[j++];
         }
     }
 }
